bef705660c05e79d2e9b62a49c01a7d8a28f0f54
Dan McCahon <dtmccahon@outlook.com>
2024-03-04 13:32:36 +0800

    1.6 - Products CSV Import
    Completed the import for the csv file. If I enable user creates later, this will need to be completely reworked. Not a bad first draft though.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import models
+import datetime
 import csv
 
 
@@ -21,10 +22,32 @@ def import_inventory_csv():
             product_name = row[0]
             product_price = row[1]
             product_quantity = row[2]
-            date_updated = row[3]
+            date_updated = make_date(row[3])
             brand_name = row[4]
             
+            cleaned_product_price = float(product_price.replace('$', ''))
+            price_in_cents = int(cleaned_product_price * 100)
+            brand_query = models.session.query(models.Brand).filter(models.Brand.brand_name==brand_name).first().brand_id
+            
+            new_product = models.Product(product_name=product_name, product_price=price_in_cents, 
+                                         product_quantity=product_quantity, product_updated=date_updated,
+                                         brand_id = brand_query)
+            
+            models.session.add(new_product)
+            models.session.commit()
+      
+
+def make_date(date_str):
+    split_date = date_str.split('/')
+    month = int(split_date[0])
+    day = int(split_date[1])
+    year = int(split_date[2])
+    return_date = datetime.date(year, month, day)
+    return return_date      
+   
     
 if __name__ == '__main__':
     models.Base.metadata.create_all(models.engine)
     import_brands_csv()
+    import_inventory_csv()
+    
