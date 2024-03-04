43827a34cb7e2cbb3dd814385f0c3a7e79c21927
Dan McCahon <dtmccahon@outlook.com>
2024-03-04 10:13:27 +0800

    1.2 - Models.Py Compelete
    Setup the database structure.

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import (create_engine, Column,
                         Integer, String, Date)
 from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
 
 engine = create_engine('sqlite:///inventory.db', echo=False)
@@ -10,8 +10,28 @@ Session = sessionmaker(engine)
 session = Session()
 
 
-# Brands
-# brand_id | brand_name
+class Brand(Base):
+    __tablename__ = "Brands"
+    brand_id = Column(Integer, primary_key=True)
+    brand_name = Column(String)
 
-# Products 
-# product_id | product_name | product_quantity | product_price | date_updated | brand_id
+    def __repr__(self):
+        return f"brand_name:{self.brand_name}"
+
+
+class Product(Base):
+    __tablename__ = "Products"
+    product_id = Column(Integer, primary_key=True)
+    product_name = Column(String)
+    product_quantity = Column(Integer)
+    product_price = Column(Integer)
+    product_updated = Column(Date)
+    brand_id = relationship("Brand", back_popualates="brand_id",
+                                    cascade="all, delete, delete-orphan")
+    
+    def __repr__(self):
+        return f'''product_name: {self.product_name},
+                   \rproduct_quantity: {self.product_quantity},
+                   \rproduct_price: {self.product_price},
+                   \rproduct_updated: {self.product_updated},
+                   \rbrand_id: {self.brand_id}'''
