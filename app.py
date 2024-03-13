9edd5adedf22f0ff7965ab777d997b4afb146f6e
Dan McCahon <dtmccahon@outlook.com>
2024-03-13 13:55:02 +0800

    1.9 - Step 15 Add Product
    Added code from my Python Techdegree Project 4 to add a Product. When I started typing it out I remembered enough of it that I thought, why reinvent the wheel.

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def import_inventory_csv():
             product_name = row[0]
             product_price = row[1]
             product_quantity = int(row[2])
-            date_updated = make_date(row[3])
+            date_updated = clean_date(row[3])
             brand_name = row[4]
             
             cleaned_product_price = float(product_price.replace('$', ''))
@@ -37,13 +37,54 @@ def import_inventory_csv():
             models.session.commit()
       
 
-def make_date(date_str):
+def clean_price(price_str):
+    try:
+        cleaned_price = float(price_str.replace('$', ''))
+    except ValueError:
+        print('\n****** PRICE ERROR ******')
+        print('Please enter a price (ex 5.99)')
+    else:
+        return int(cleaned_price * 100)
+
+
+def clean_quantity(quantity_str):
+    try:
+        cleaned_quantity = int(quantity_str)
+    except ValueError:
+        print('\n****** QUANTITY ERROR ******')
+        print('Please enter a quantity (ex 5)')
+    else:
+        return int(cleaned_quantity)
+
+
+def clean_date(date_str):
     split_date = date_str.split('/')
-    month = int(split_date[0])
-    day = int(split_date[1])
-    year = int(split_date[2])
-    return_date = datetime.date(year, month, day)
-    return return_date      
+    try:
+        month = int(split_date[0])
+        day = int(split_date[1])
+        year = int(split_date[2])
+        return_date = datetime.date(year, month, day)
+    except (ValueError, IndexError):
+        print('\n****** DATE ERROR ******')
+        print('Please enter a date (ex 04/08/2021)')
+    else: 
+        return return_date
+
+
+def add_product(new_product):
+    existing_product = models.session.query(models.Product).filter(models.Product.product_name==new_product.product_name).first()
+    if existing_product:
+        if existing_product.product_updated < new_product.product_updated:
+            existing_product.product_name = new_product.product_name
+            existing_product.product_price = new_product.product_price
+            existing_product.product_quantity = new_product.product_quantity
+            existing_product.product_updated = new_product.product_updated
+            models.session.commit()
+            print("\nProduct Updated\n")
+    else:
+        models.session.add(new_product)
+        models.session.commit()
+        print("\nProduct Added\n")
 
 
 def get_product_by_id():
@@ -62,6 +103,35 @@ def get_product_by_id():
             break
 
 
+def user_entered_product():
+    product_name = input("\nProduct Name: ")
+    price_error = True
+    while price_error:
+        price = input("Product Price: ")
+        price = clean_price(price)
+        if type(price) == int:
+            price_error = False
+    quantity_error = True
+    while quantity_error:
+        quantity = input("Product Quantity: ")
+        quantity = clean_quantity(quantity)
+        if type(quantity) == int:
+            quantity_error = False
+    date_error = True
+    while date_error:
+        date = input("Product Date Updated (ex 04/08/2021): ")
+        date = clean_date(date)
+        if type(date) == datetime.date:
+            date_error = False
+    brand_id = input("Brand ID:")
+    new_product = models.Product(product_name=product_name,
+                        product_price=price,
+                        product_quantity=quantity,
+                        product_updated=date,
+                        brand_id=brand_id)
+    return new_product
+
+
 def menu():
     print("Welcome\n")
     while True:
@@ -69,7 +139,8 @@ def menu():
         if user_input == "v":
             get_product_by_id()
         elif user_input == "n":
-            print("Add New Product")
+            new_product = user_entered_product()
+            add_product(new_product) 
         elif user_input == "a":
             print("View Anaylsis")
         elif user_input == "b":
